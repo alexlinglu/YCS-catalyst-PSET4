3388ec25ffe9bb0e78e629e61e6e9f6b2343a1c2
Alex Lu <alexlinglu@gmail.com>
2024-11-08 23:04:12 -0500

    pset submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 const courses = [
   { id: 1, name: "Introduction to Computer Science", code: "CPSC 201", credits: 4 },
@@ -9,12 +10,41 @@ const courses = [
 ];
 
 function App() {
+  let courseElements = courses.map((element) => <li key={element.id} className="list">
+    <span className="list-element">{element.name}</span>
+    <span className="list-element">{element.code}</span>
+    <span className="list-element">{element.credits}</span>
+    <button className="button" onClick={() => {addCourse(element); updateCredits(element.credits)} }>Add</button>
+    </li>);
   // TODO: Implement state for selectedCourses
-
+  let [courseList, changeCourseList] = useState([]);
+  let courseListElements = courseList.map((element) => <li key={element.id} className="list">
+  <span className="list-element">{element.name}</span>
+  <span className="list-element">{element.code}</span>
+  <span className="list-element">{element.credits}</span>
+  <button className="button" onClick={() => {removeCourse(element); updateCredits(-1 * element.credits)}}>Remove</button>
+  </li>);
   // TODO: Implement addCourse function
-
+  function addCourse(course){
+    let newCourseList = [...courseList];
+    newCourseList.push(course);
+    changeCourseList(newCourseList);
+  }
   // TODO: Implement removeCourse function
-
+  function removeCourse(course){
+    let newCourseList = [...courseList];
+    let index = newCourseList.indexOf(course);
+    newCourseList = newCourseList.toSpliced(index, 1);
+    changeCourseList(newCourseList);
+  }
+  let [courseHours, changeCourseHours] = useState(0);
+  function updateCredits(currentCredits){
+    let newCourseHours = currentCredits;
+    courseList.forEach((course) => {
+      newCourseHours = course.credits + newCourseHours;
+    })
+    changeCourseHours(newCourseHours);
+  }
   return (
     <div className="App">
       <h1>Yale Course Scheduler</h1>
@@ -22,12 +52,17 @@ function App() {
       <div className="course-list">
         <h2>Available Courses</h2>
         {/* TODO: Display the list of available courses */}
+        <ul >{courseElements}</ul>
       </div>
       
       <div className="schedule">
         <h2>My Schedule</h2>
         {/* TODO: Display the selected courses */}
+        <ul id="course-schedule">
+          {courseListElements}
+        </ul>
         {/* TODO: Display the total credit hours */}
+          Total Credits: {courseHours}
       </div>
     </div>
   );
